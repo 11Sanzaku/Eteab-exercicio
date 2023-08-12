8eddd52db863409e8e854fec6654872c45f1b3a6
Pedro <pedrohjac28@gmail.com>
2023-08-11 21:40:11 -0300

    Haikai

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Na despedida");
+Console.WriteLine("as costas! Solitário");
+Console.WriteLine("vento de outono.");
+Console.WriteLine("");
+Console.WriteLine("Matsuo Bashô");
